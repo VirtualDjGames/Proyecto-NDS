1a3244acb081b079cc70eca5521730aaa120181d
JoseIgnacioMorr <nachocolt99@gmail.com>
2023-09-26 01:34:14 -0300

    Avance: Movimiento 3D (Inputs de Mando incluido)
    -Caminar
    -Correr
    -Saltar
    -Mirar (Rotar)

---
+++ Assets/NDS/Jose Ignacio Morr/Scripts/Movimiento.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movimiento : MonoBehaviour
+{
+    CharacterController characterController;
+    private InputsMap inputs;
+    
+    public float walkSpeed = 6.0f;
+    public float runSpeed = 10.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    
+    public Camera cam;
+    private float mouseHorizontal = 3.0f;
+    private float mouseVertical = 2.0f;
+
+
+    private float minRotation = -65.0f;
+    private float maxRotation = 60.0f;
+    float h_mouse, v_mouse;
+
+
+    private Vector3 move = Vector3.zero;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        inputs = new InputsMap();
+        inputs.Gameplay.Enable();
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    void Update()
+    {
+        h_mouse = mouseHorizontal * Input.GetAxis("Mouse X");
+        v_mouse += mouseVertical * Input.GetAxis("Mouse Y");
+
+        v_mouse = Mathf.Clamp(v_mouse, minRotation, maxRotation);
+
+
+        cam.transform.localEulerAngles = new Vector3(-v_mouse, 0, 0);
+
+        transform.Rotate(0, h_mouse, 0);
+
+
+        if (characterController.isGrounded)
+        {
+            move = inputs.Gameplay.Walk.ReadValue<Vector3>();
+
+
+            if (inputs.Gameplay.Run.IsPressed())
+            {
+                move = transform.TransformDirection(move) * runSpeed;
+            }
+            else
+            {
+                move = transform.TransformDirection(move) * walkSpeed;
+            }
+
+            if (inputs.Gameplay.Jump.WasPressedThisFrame())
+            {
+                move.y = jumpSpeed;
+            }
+        }
+        move.y -= gravity * Time.deltaTime;
+
+        characterController.Move(move * Time.deltaTime);
+    }
+}
