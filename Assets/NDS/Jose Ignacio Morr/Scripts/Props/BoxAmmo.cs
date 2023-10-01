2eae874e3d069b86840176f265c20a6dd3043a24
JoseIgnacioMorr <nachocolt99@gmail.com>
2023-10-01 20:22:02 -0300

    Avance: Interacción Llave-Puerta, Botiquin y Munición.

---
+++ Assets/NDS/Jose Ignacio Morr/Scripts/Props/BoxAmmo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BoxAmmo : MonoBehaviour
+{
+    //public AudioSource ammoSound;
+
+    void Update()
+    {
+        transform.Rotate(0, 1, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<CharacterController>())
+        {
+            //Añadir munición
+            Destroy(gameObject);
+        }
+    }
+}
+
