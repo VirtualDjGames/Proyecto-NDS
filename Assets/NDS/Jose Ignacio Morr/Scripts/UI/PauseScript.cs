9e12396c88585f5ced8e44ecdc90a6999dbdfcf7
JoseIgnacioMorr <nachocolt99@gmail.com>
2023-10-02 17:58:25 -0300

    Avance: Fonts + Detalles del Menú

--- Assets/NDS/Jose Ignacio Morr/Scripts/UI/PauseScript.cs
+++ Assets/NDS/Jose Ignacio Morr/Scripts/UI/PauseScript.cs
@@ -8,6 +8,9 @@ using UnityEngine.SceneManagement;
 public class PauseScript : MonoBehaviour
 {
     private InputsMap inputs;
+    private bool isPaused = false;
+    public CanvasGroup pausePanel;
+    public CanvasGroup hudPanel;
 
     void Start()
     {
@@ -21,20 +24,59 @@ public class PauseScript : MonoBehaviour
     {
         if (inputs.Gameplay.Pause.WasPressedThisFrame())
         {
-            inputs.Gameplay.Disable();
-            inputs.UI.Enable();
-            Cursor.lockState = CursorLockMode.None;
-
-            Time.timeScale = 0;
+            Paused();
         }
 
         if (inputs.UI.Resume.WasPressedThisFrame())
         {
-            inputs.UI.Disable();
-            inputs.Gameplay.Enable();
-            Cursor.lockState = CursorLockMode.Locked;
-
-            Time.timeScale = 1;
+            Resume();
         }
+
+        if(isPaused)
+        {
+            hudPanel.alpha -= Time.unscaledDeltaTime * 4;
+            pausePanel.alpha += Time.unscaledDeltaTime * 4;
+            hudPanel.interactable = false;
+            pausePanel.interactable = true;
+
+            if (hudPanel.alpha == 0f && pausePanel.alpha == 1f)
+            {
+                hudPanel.alpha = 0f;    
+                pausePanel.alpha = 1f;
+            }
+        }
+        else
+        {
+            hudPanel.alpha += Time.unscaledDeltaTime * 4;
+            pausePanel.alpha -= Time.unscaledDeltaTime * 4;
+            hudPanel.interactable = true;
+            pausePanel.interactable = false;
+
+            if (hudPanel.alpha == 1f && pausePanel.alpha == 0f)
+            {
+                hudPanel.alpha = 1f;
+                pausePanel.alpha = 0f;
+            }
+        }
+    }
+
+    private void Paused()
+    {
+        inputs.Gameplay.Disable();
+        inputs.UI.Enable();
+        isPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+
+        Time.timeScale = 0;
+    }    
+
+    public void Resume()
+    {
+        inputs.UI.Disable();
+        inputs.Gameplay.Enable();
+        isPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        Time.timeScale = 1;
     }
 }
