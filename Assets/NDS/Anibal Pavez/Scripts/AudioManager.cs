5b14d5c0e5c26d32bb973193cc2dd90533f36b29
AnibAkan <anibal.pavez7123@gmail.com>
2023-10-01 22:56:15 -0300

    SoundSteps
    Codigo y sonido de pasos, inicial

---
+++ Assets/NDS/Anibal Pavez/Scripts/AudioManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+	public static AudioManager Instance;
+	public AudioSource audioSourceSFX; //Efectos de sonido
+	public AudioSource audioSourceMusic; //Musica
+	public AudioSource audioSourceAmbient; //Ambiente
+										   //private AudioSource audioSourceAmbient; //Sonido ambiente EJ:LLuvia
+	public AudioMixer master;
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			//audioSourceAmbient = gameObject.AddComponent<AudioSource>();
+		}
+		else
+		{
+			Destroy(Instance);
+			Debug.LogError("Mas de una instancia");
+			Debug.LogError("Revisa tu escena >:(");
+		}
+
+	}
+	public void PlayGlobalSoundEffect(AudioClip clip, float volume = 1f)
+	{
+		audioSourceSFX.PlayOneShot(clip, volume);
+	}
+	public void PlayMusic(AudioClip clip)
+	{
+
+		audioSourceMusic.clip = clip;
+		audioSourceMusic.Play();
+	}
+
+	public void PlayAmbient(AudioClip clip, bool loop = true)
+	{
+		audioSourceAmbient.loop = loop;
+		audioSourceAmbient.clip = clip;
+		audioSourceAmbient.Play();
+    }
+    public void VolumeAmbient(float volume)
+    {
+        audioSourceAmbient.volume = volume;
+    }
+	//Manejo de Volumen por UI, haciendo uso de AudioMixer
+    public void SetSoundEffectsVolume(float volume)
+	{
+		master.SetFloat("volSFX", Mathf.Log10(volume) * 20);
+	}
+	public void SetMusicVolume(float volume)
+	{
+		master.SetFloat("volMusic", Mathf.Log10(volume) * 20);
+	}
+	public void SetAmbientVolume(float volume)
+	{
+		master.SetFloat("volAmbient", Mathf.Log10(volume) * 20);
+	}
+}
+
