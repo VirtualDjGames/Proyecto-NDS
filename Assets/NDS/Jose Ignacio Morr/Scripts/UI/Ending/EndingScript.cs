eec1441df6b7f77526fc6e2b620a9aa8fdaaa0f0
JoseIgnacioMorr <nachocolt99@gmail.com>
2023-10-13 01:21:39 -0300

    Avance: Ending Scene

---
+++ Assets/NDS/Jose Ignacio Morr/Scripts/UI/Ending/EndingScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingScript : MonoBehaviour
+{
+    public GameObject finishAdviser;
+
+    private float finishTime;
+    private bool isFinish = false;
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+
+    void Update()
+    {
+        finishTime += Time.deltaTime;
+
+        if(finishTime >= 20f)
+        { 
+            isFinish = true;
+        }
+
+        if (isFinish)
+        {
+            finishAdviser.SetActive(true);
+
+            if (Input.anyKey)
+            {
+                SceneManager.LoadScene("Menu");
+            }
+        }
+    }
+}
